e7fc3edb48c6904cf10a8035765767a845dc8d90
Rafael <rafaelfelipeac@gmail.com>
2021-01-17 11:59:55 -0300

    Added more unit tests for RepositoryDtoMapper.

--- app/src/test/java/com/rafaelfelipeac/githubrepositories/features/repositories/data/model/RepositoryDtoMapperTest.kt
+++ app/src/test/java/com/rafaelfelipeac/githubrepositories/features/repositories/data/model/RepositoryDtoMapperTest.kt
@@ -32,4 +32,69 @@ class RepositoryDtoMapperTest {
         // then
         result equalTo other
     }
+
+    @Test
+    fun `WHEN name is null THEN mapped name is empty`() {
+        // when
+        val result = repositoryDtoMapper.map(
+            createRepositoryDto(
+                name = null,
+            )
+        )
+
+        // then
+        result.name equalTo ""
+    }
+
+    @Test
+    fun `WHEN stars is null THEN mapped stars is empty`() {
+        // when
+        val result = repositoryDtoMapper.map(
+            createRepositoryDto(
+                stars = null,
+            )
+        )
+
+        // then
+        result.stars equalTo 0
+    }
+
+    @Test
+    fun `WHEN forks is null THEN mapped forks is empty`() {
+        // when
+        val result = repositoryDtoMapper.map(
+            createRepositoryDto(
+                forks = null
+            )
+        )
+
+        // then
+        result.forks equalTo 0
+    }
+
+    @Test
+    fun `WHEN login is null THEN mapped ownerName is empty`() {
+        // when
+        val result = repositoryDtoMapper.map(
+            createRepositoryDto(
+                login = null
+            )
+        )
+
+        // then
+        result.owner.name equalTo ""
+    }
+
+    @Test
+    fun `WHEN avatar_url is null THEN mapped ownerAvatarUrl is empty`() {
+        // when
+        val result = repositoryDtoMapper.map(
+            createRepositoryDto(
+                avatar_url = null,
+            )
+        )
+
+        // then
+        result.owner.avatarUrl equalTo ""
+    }
 }
